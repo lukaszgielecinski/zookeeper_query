5572ea2d848e079c8b7df83129ed672118f17102
Lukasz Gielecinski <lukasz.gielecinski@gmail.com>
2016-09-21 23:42:30 +0100

    output zookeeper queries

--- src/main/java/ZookeeperQuery.java
+++ src/main/java/ZookeeperQuery.java
@@ -6,7 +6,7 @@ import org.apache.zookeeper.ZooKeeper;
 import java.util.List;
 
 public class ZookeeperQuery {
-    static{
+    static {
         Logger.getRootLogger().setLevel(Level.OFF);
     }
 
@@ -20,24 +20,29 @@ public class ZookeeperQuery {
         ZooKeeper zk = new ZooKeeper("192.168.99.100:2181", 10000, null);
         if ("leader".equals(command)) {
             String topicName = args[1];
-            String query = "/brokers/topics/" +topicName+"/partitions/0/state";
+            String query = "/brokers/topics/" + topicName + "/partitions/0/state";
             int leader = JsonPath.parse(new String(zk.getData(query, false, null))).read("$.leader");
             System.out.println(leader);
         } else if ("describeTopic".equals(command)) {
             String topicName = args[1];
-            String query = "/brokers/topics/" +topicName+"/partitions/0/state";
+            String query = "/brokers/topics/" + topicName + "/partitions/0/state";
+            System.out.println("Executing zookeeper: get " + query);
             System.out.println(new String(zk.getData(query, false, null)));
         } else if ("brokers".equals(command)) {
+            System.out.println("Executing zookeeper: get /brokers/ids");
             List<String> ids = zk.getChildren("/brokers/ids", false);
             for (String id : ids) {
-                String brokerInfo = new String(zk.getData("/brokers/ids/" + id, false, null));
+                String brokerPath = "/brokers/ids/" + id;
+                System.out.println("Executing zookeeper: get " + brokerPath);
+                String brokerInfo = new String(zk.getData(brokerPath, false, null));
                 System.out.println(id + ": " + brokerInfo);
             }
         } else {
+            System.out.println("Executing zookeeper: get " + command);
             List<String> availableCommands = zk.getChildren(command, false);
-            if(availableCommands.isEmpty()){
+            if (availableCommands.isEmpty()) {
                 System.out.println(new String(zk.getData(command, false, null)));
-            }else {
+            } else {
                 System.out.println(availableCommands);
             }
         }
